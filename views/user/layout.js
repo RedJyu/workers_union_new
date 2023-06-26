4e9142051fdf193431f99bb757fe442ecfb98be3
RedJyu <piotr.stezycki@protonmai.com>
2023-06-26 15:11:14 +0200

    css

--- views/user/layout.js
+++ views/user/layout.js
@@ -7,9 +7,10 @@ export const layoutUser = ({ content }) => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="/main.css">
+   
 </head>
 <body>
-<div class="container">
+<div class="container flow">
     ${content}
 </div>
 </body>
